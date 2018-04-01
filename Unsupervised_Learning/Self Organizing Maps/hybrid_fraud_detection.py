7e1d5a7a7f0fbf48c5ef41b7ed51646962ea8b76
Matt Escobar <mattoescobar@gmail.com>
2018-04-01 15:52:17 +0900

    Change SOM and hybrid model to spot frauds more consistently
    SOM running 100 times to spot more consistent fraudulent sets

--- Unsupervised_Learning/Self Organizing Maps/hybrid_fraud_detection.py
+++ Unsupervised_Learning/Self Organizing Maps/hybrid_fraud_detection.py
@@ -19,36 +19,32 @@ from sklearn.preprocessing import MinMaxScaler
 sc = MinMaxScaler(feature_range = (0, 1))
 X = sc.fit_transform(X)
 
-# Training the SOM
+# Training the SOM 100 times to spot frauds more consistently
 from minisom import MiniSom
-som = MiniSom(x = 10, y = 10, input_len = 15, sigma = 1.0, learning_rate = 0.5)
-som.random_weights_init(X)
-som.train_random(data = X, num_iteration = 100)
+frauds = np.array(None)
+for i in range(100):
+    som = MiniSom(x = 10, y = 10, input_len = 15, sigma = 1.0, learning_rate = 0.5)
+    som.random_weights_init(X)
+    som.train_random(data = X, num_iteration = 100)
 
-# Visualizing the results
-from pylab import bone, pcolor, colorbar, plot, show
-bone()
-pcolor(som.distance_map().T)
-colorbar()
-markers = ['o', 's']
-colors = ['r', 'g']
-for i, x in enumerate(X):
-    w = som.winner(x)
-    plot(w[0] + 0.5,
-         w[1] + 0.5,
-         markers[y[i]],
-         markeredgecolor = colors[y[i]],
-         markerfacecolor = 'None',
-         markersize = 10,
-         markeredgewidth = 2)
-show()
+    # Finding the frauds
+    mappings = som.win_map(X)
 
-# Finding the frauds
-mappings = som.win_map(X)
-frauds = np.concatenate((mappings[(5,3)], mappings[(8,3)]), axis = 0)
+    # Finding the coordinates with maximum normalised distance -> Outliers
+    outlier_mappings = np.unravel_index(som.distance_map().T.argmax(), som.distance_map().T.shape)
+    outlier_mappings = outlier_mappings[::-1]
+
+    frauds_now = mappings[(outlier_mappings[0], outlier_mappings[1])]
+
+    if not frauds:
+        frauds = frauds_now
+    else:
+        if frauds_now:
+            frauds = np.concatenate((frauds, frauds_now), axis=0).tolist()
+    print(i)
+
+frauds = np.unique(frauds, axis=0)
 frauds = sc.inverse_transform(frauds)
-
-
 
 # Part 2 - Going from Unsupervised to Supervised Deep Learning
 
