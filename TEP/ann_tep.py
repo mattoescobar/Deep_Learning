eed53524951300c9b68d4adedba6dfbdec8fca77
Matt Escobar <mattoescobar@gmail.com>
2018-04-08 20:15:33 +0900

    Add for loop to save ROC data for ann and rnn applied to TEP

--- TEP/ann_tep.py
+++ TEP/ann_tep.py
@@ -1,6 +1,4 @@
-# Artificial Neural Network
-
-
+# Artificial Neural Network applied to Tennessee Eastman Process (TEP)
 
 # Part 1 - Data Preprocessing
 
@@ -10,83 +8,104 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import scipy.io as sio
 
-# Importing the training set
-dataset = sio.loadmat('TEP_9.mat')
-dataset_train = np.concatenate([dataset['X0'], dataset['X']], axis=0)
-y_target_train = np.zeros(dataset_train.shape[0])
-y_target_train[480:] = 1
+# Initialising false positive rate and true positive rate arrays
+fpr_arr = []
+tpr_arr = []
+roc_arr = []
 
-# Feature Scaling
-from sklearn.preprocessing import MinMaxScaler
-sc = MinMaxScaler(feature_range = (0, 1))
-training_set_scaled = sc.fit_transform(dataset_train)
+# Calculating results for 6 faults (1, 2, 5, 8, 9, 13)
+for i in [1, 2, 5, 8, 9, 13]:
 
-X_train, y_train = np.array(training_set_scaled), np.array(y_target_train)
+    # Importing the training set
+    dataset = sio.loadmat("TEP_%s.mat" % str(i))
+    dataset_train = np.concatenate([dataset['X0'], dataset['X']], axis=0)
+    time_train = np.array(range(0, dataset_train.shape[0]))
+    y_target_train = np.zeros(dataset_train.shape[0])
+    y_target_train[480:] = 1
 
-# Part 2 - Building the RNN
+    # Feature Scaling
+    from sklearn.preprocessing import MinMaxScaler
+    sc = MinMaxScaler(feature_range = (0, 1))
+    training_set_scaled = sc.fit_transform(dataset_train)
 
-# Importing the Keras libraries and packages
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-from keras.layers import Dropout
+    # # Visualising TEP faults
+    # plt.figure()
+    # plt.plot(time_train, training_set_scaled)
+    # plt.show()
 
-# Initialising the RNN
-classifier = Sequential()
+    X_train, y_train = np.array(training_set_scaled), np.array(y_target_train)
 
-# Adding the input layer and the first hidden layer
-classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = X_train.shape[1]))
+    # Part 2 - Building the RNN
 
-# Adding the second hidden layer
-classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+    # Importing the Keras libraries and packages
+    from keras.models import Sequential
+    from keras.layers import Dense
+    from keras.layers import LSTM
+    from keras.layers import Dropout
 
-# Adding the output layer
-classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+    # Initialising the RNN
+    classifier = Sequential()
 
-# Compiling the ANN
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+    # Adding the input layer and the first hidden layer
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = X_train.shape[1]))
 
-# Fitting the ANN to the Training set
-classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
+    # Adding the second hidden layer
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+
+    # Adding the output layer
+    classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+
+    # Compiling the ANN
+    classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+    # Fitting the ANN to the Training set
+    classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
 
 
-# Part 3 - Making the predictions and visualising the results
+    # Part 3 - Making the predictions and visualising the results
 
-# Getting the test target values
-dataset_test = dataset['Xt']
-y_target_test = np.zeros(dataset_test.shape[0])
-y_target_test[160:] = 1
+    # Getting the test target values
+    dataset_test = dataset['Xt']
+    y_target_test = np.zeros(dataset_test.shape[0])
+    y_target_test[160:] = 1
 
-# Visualising test data
-# plt.figure()
-# plt.plot(time_test, dataset_test[:,0])
-# plt.show()
+    # Visualising test data
+    # plt.figure()
+    # plt.plot(time_test, dataset_test[:,0])
+    # plt.show()
 
-# Feature Scaling and data structure
-test_set_scaled = sc.transform(dataset_test)
-X_test = np.array(test_set_scaled)
-y_test = np.array(y_target_test)
-predicted_state = classifier.predict(X_test)
+    # Feature Scaling and data structure
+    test_set_scaled = sc.transform(dataset_test)
+    X_test = np.array(test_set_scaled)
+    y_test = np.array(y_target_test)
+    predicted_state = classifier.predict(X_test)
 
-# Visualising the results
-plt.plot(y_test, color = 'red', label = 'TEP anomaly label')
-plt.plot(predicted_state, color = 'blue', label = 'TEP fault detection')
-plt.title('TEP fault detection ')
-plt.xlabel('Time')
-plt.ylabel('Anomalous behaviour likelihood')
-plt.legend()
-plt.show()
+    # Visualising performance through ROC curve
+    import sklearn.metrics as skm
+    fpr, tpr, threshold = skm.roc_curve(y_test, predicted_state)
+    roc_auc = skm.auc(fpr, tpr)
+    # plt.title('Receiver Operating Characteristic')
+    # plt.plot(fpr, tpr, 'b', label = 'AUC = %0.2f' % roc_auc)
+    # plt.legend(loc = 'lower right')
+    # plt.plot([0, 1], [0, 1],'r--')
+    # plt.xlim([0, 1])
+    # plt.ylim([0, 1])
+    # plt.ylabel('True Positive Rate')
+    # plt.xlabel('False Positive Rate')
+    # plt.show()
 
-# Visualising performance through ROC curve
-import sklearn.metrics as skm
-fpr, tpr, threshold = skm.roc_curve(y_test, predicted_state)
-roc_auc = skm.auc(fpr, tpr)
-plt.title('Receiver Operating Characteristic')
-plt.plot(fpr, tpr, 'b', label = 'AUC = %0.2f' % roc_auc)
-plt.legend(loc = 'lower right')
-plt.plot([0, 1], [0, 1],'r--')
-plt.xlim([0, 1])
-plt.ylim([0, 1])
-plt.ylabel('True Positive Rate')
-plt.xlabel('False Positive Rate')
-plt.show()
+    # # Visualising the predicted results
+    # plt.plot(y_test, color = 'red', label = 'TEP anomaly label')
+    # plt.plot(predicted_state, color = 'blue', label = 'TEP fault detection')
+    # plt.title('TEP fault detection ')
+    # plt.xlabel('Time')
+    # plt.ylabel('Anomalous behaviour likelihood')
+    # plt.legend()
+    # plt.show()
+
+    # Saving results for comparison later
+    fpr_arr.append(fpr)
+    tpr_arr.append(tpr)
+    roc_arr.append(roc_auc)
+
+np.savez('ann_roc', fpr_arr, tpr_arr, roc_arr)
--- TEP/rnn_tep.py
+++ TEP/rnn_tep.py
@@ -1,4 +1,4 @@
-# Recurrent Neural Network
+# Recurrent Neural Network applied to Tennessee Eastman Process (TEP)
 
 
 
@@ -10,115 +10,129 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import scipy.io as sio
 
-# Importing the training set
-dataset = sio.loadmat('TEP_21.mat')
-dataset_train = np.concatenate([dataset['X0'], dataset['X']], axis=0)
-time_train = np.array(range(0, dataset_train.shape[0]))
-y_target_train = np.zeros(dataset_train.shape[0])
-y_target_train[480:] = 1
+# Initialising false positive rate and true positive rate arrays
+fpr_arr = []
+tpr_arr = []
+roc_arr = []
 
-# Feature Scaling
-from sklearn.preprocessing import MinMaxScaler
-sc = MinMaxScaler(feature_range = (0, 1))
-training_set_scaled = sc.fit_transform(dataset_train)
+# Calculating results for 6 faults (1, 2, 5, 8, 9, 13)
+for i in [1, 2, 5, 8, 9, 13]:
 
-# # Visualising TEP faults
-# plt.figure()
-# plt.plot(time_train, training_set_scaled)
-# plt.show()
+    # Importing the training set
+    dataset = sio.loadmat("TEP_%s.mat" % str(i))
+    dataset_train = np.concatenate([dataset['X0'], dataset['X']], axis=0)
+    time_train = np.array(range(0, dataset_train.shape[0]))
+    y_target_train = np.zeros(dataset_train.shape[0])
+    y_target_train[480:] = 1
 
-# Creating a data structure with X timesteps and 1 output
-X_train = []
-y_train = []
-time_step = 15
-for i in range(time_step, training_set_scaled.shape[0]):
-    X_train.append(training_set_scaled[i-time_step:i, :])
-    y_train.append(y_target_train[i])
-X_train, y_train = np.array(X_train), np.array(y_train)
+    # Feature Scaling
+    from sklearn.preprocessing import MinMaxScaler
+    sc = MinMaxScaler(feature_range = (0, 1))
+    training_set_scaled = sc.fit_transform(dataset_train)
 
-# Part 2 - Building the RNN
+    # # Visualising TEP faults
+    # plt.figure()
+    # plt.plot(time_train, training_set_scaled)
+    # plt.show()
 
-# Importing the Keras libraries and packages
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-from keras.layers import Dropout
+    # Creating a data structure with X timesteps and 1 output
+    X_train = []
+    y_train = []
+    time_step = 15
+    for i in range(time_step, training_set_scaled.shape[0]):
+        X_train.append(training_set_scaled[i-time_step:i, :])
+        y_train.append(y_target_train[i])
+    X_train, y_train = np.array(X_train), np.array(y_train)
 
-# Initialising the RNN
-regressor = Sequential()
+    # Part 2 - Building the RNN
 
-# Adding the first LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], X_train.shape[2])))
-regressor.add(Dropout(0.2))
+    # Importing the Keras libraries and packages
+    from keras.models import Sequential
+    from keras.layers import Dense
+    from keras.layers import LSTM
+    from keras.layers import Dropout
 
-# Adding a second LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True))
-regressor.add(Dropout(0.2))
+    # Initialising the RNN
+    regressor = Sequential()
 
-# Adding a third LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True))
-regressor.add(Dropout(0.2))
+    # Adding the first LSTM layer and some Dropout regularisation
+    regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], X_train.shape[2])))
+    regressor.add(Dropout(0.2))
 
-# Adding a fourth LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50))
-regressor.add(Dropout(0.2))
+    # Adding a second LSTM layer and some Dropout regularisation
+    regressor.add(LSTM(units = 50, return_sequences = True))
+    regressor.add(Dropout(0.2))
 
-# Adding the output layer
-regressor.add(Dense(units = 1))
+    # Adding a third LSTM layer and some Dropout regularisation
+    regressor.add(LSTM(units = 50, return_sequences = True))
+    regressor.add(Dropout(0.2))
 
-# Compiling the RNN
-regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+    # Adding a fourth LSTM layer and some Dropout regularisation
+    regressor.add(LSTM(units = 50))
+    regressor.add(Dropout(0.2))
 
-# Fitting the RNN to the Training set
-regressor.fit(X_train, y_train, epochs = 10, batch_size = 32)
+    # Adding the output layer
+    regressor.add(Dense(units = 1))
 
+    # Compiling the RNN
+    regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
 
-
-# Part 3 - Making the predictions and visualising the results
-
-# Getting the test target values
-dataset_test = np.concatenate([dataset_train[0:time_step, :], dataset['Xt']], axis=0)
-time_test = np.array(range(0, dataset_test.shape[0]))
-y_target_test = np.zeros(dataset_test.shape[0])
-y_target_test[160+time_step:] = 1
-
-# Visualising test data
-# plt.figure()
-# plt.plot(time_test, dataset_test[:,0])
-# plt.show()
-
-# Feature Scaling and data structure
-test_set_scaled = sc.transform(dataset_test)
-X_test = []
-y_test = []
-for i in range(time_step, dataset_test.shape[0]):
-    X_test.append(test_set_scaled[i-time_step:i, :])
-    y_test.append(y_target_test[i])
-X_test = np.array(X_test)
-X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], X_test.shape[2]))
-
-predicted_state = regressor.predict(X_test)
+    # Fitting the RNN to the Training set
+    regressor.fit(X_train, y_train, epochs = 10, batch_size = 32)
 
 
-# Visualising the results
-plt.plot(y_test, color = 'red', label = 'TEP anomaly label')
-plt.plot(predicted_state, color = 'blue', label = 'TEP fault detection')
-plt.title('TEP fault detection ')
-plt.xlabel('Time')
-plt.ylabel('Anomalous behaviour likelihood')
-plt.legend()
-plt.show()
 
-# Visualising performance through ROC curve
-import sklearn.metrics as skm
-fpr, tpr, threshold = skm.roc_curve(y_test, predicted_state)
-roc_auc = skm.auc(fpr, tpr)
-plt.title('Receiver Operating Characteristic')
-plt.plot(fpr, tpr, 'b', label = 'AUC = %0.2f' % roc_auc)
-plt.legend(loc = 'lower right')
-plt.plot([0, 1], [0, 1],'r--')
-plt.xlim([0, 1])
-plt.ylim([0, 1])
-plt.ylabel('True Positive Rate')
-plt.xlabel('False Positive Rate')
-plt.show()
+    # Part 3 - Making the predictions and visualising the results
+
+    # Getting the test target values
+    dataset_test = np.concatenate([dataset_train[0:time_step, :], dataset['Xt']], axis=0)
+    time_test = np.array(range(0, dataset_test.shape[0]))
+    y_target_test = np.zeros(dataset_test.shape[0])
+    y_target_test[160+time_step:] = 1
+
+    # Visualising test data
+    # plt.figure()
+    # plt.plot(time_test, dataset_test[:,0])
+    # plt.show()
+
+    # Feature Scaling and data structure
+    test_set_scaled = sc.transform(dataset_test)
+    X_test = []
+    y_test = []
+    for i in range(time_step, dataset_test.shape[0]):
+        X_test.append(test_set_scaled[i-time_step:i, :])
+        y_test.append(y_target_test[i])
+    X_test = np.array(X_test)
+    X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], X_test.shape[2]))
+
+    predicted_state = regressor.predict(X_test)
+
+    # Visualising performance through ROC curve
+    import sklearn.metrics as skm
+    fpr, tpr, threshold = skm.roc_curve(y_test, predicted_state)
+    roc_auc = skm.auc(fpr, tpr)
+    # plt.title('Receiver Operating Characteristic')
+    # plt.plot(fpr, tpr, 'b', label = 'AUC = %0.2f' % roc_auc)
+    # plt.legend(loc = 'lower right')
+    # plt.plot([0, 1], [0, 1],'r--')
+    # plt.xlim([0, 1])
+    # plt.ylim([0, 1])
+    # plt.ylabel('True Positive Rate')
+    # plt.xlabel('False Positive Rate')
+    # plt.show()
+    #
+    # # Visualising the predicted results
+    # plt.plot(y_test, color = 'red', label = 'TEP anomaly label')
+    # plt.plot(predicted_state, color = 'blue', label = 'TEP fault detection')
+    # plt.title('TEP fault detection ')
+    # plt.xlabel('Time')
+    # plt.ylabel('Anomalous behaviour likelihood')
+    # plt.legend()
+    # plt.show()
+
+    # Saving results for comparison later
+    fpr_arr.append(fpr)
+    tpr_arr.append(tpr)
+    roc_arr.append(roc_auc)
+
+np.savez('rnn_roc', fpr_arr, tpr_arr, roc_arr)
